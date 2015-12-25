a2c8e495aa5ae9679e7e29244949cbf9c48dd4da
Kev575 <phoenix.games.minecraft@gmail.com>
2015-12-25 20:26:34 +0100

    replace § with $ and .gitignore

--- src/kev575/adminhelper/Main.java
+++ src/kev575/adminhelper/Main.java
@@ -3,6 +3,7 @@ package kev575.adminhelper;
 import kev575.adminhelper.api.AdminHelperAPI;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -28,72 +29,72 @@ public class Main extends JavaPlugin {
 			if (args.length == 0) {
 				if (sender instanceof Player) {
 					Player p = (Player) sender;
-					if (!p.hasPermission("adminhelper.getip")) {p.sendMessage("§cYou don't have the permission to do that.");return true;}
-					p.sendMessage("§3IPv4 of §e" + p.getName() + "§8: §a" + new AdminHelperAPI(this).getIP(p));
+					if (!p.hasPermission("adminhelper.getip")) {p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cYou don't have the permission to do that."));return true;}
+					p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$3IPv4 of $e" + p.getName() + "$8: $a" + new AdminHelperAPI(this).getIP(p)));
 				} else {
 					sender.sendMessage("/getip <Player>");
 				}
 			} else if (args.length == 1) {
-				if (!sender.hasPermission("adminhelper.getip.others")) {sender.sendMessage("§cYou don't have the permission to do that.");return true;}
+				if (!sender.hasPermission("adminhelper.getip.others")) {sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cYou don't have the permission to do that."));return true;}
 				OfflinePlayer of = Bukkit.getOfflinePlayer(args[0]);
 				if (!of.hasPlayedBefore()) {
-					sender.sendMessage("§cThis player never played before on this server.");
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cThis player never played before on this server."));
 					return true;
 				}
 				if (!of.isOnline()) {
-					sender.sendMessage("§cThis player is not online.");
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cThis player is not online."));
 					return true;
 				}
 				Player p = (Player) of;
-				p.sendMessage("§3IPv4 of §e" + p.getName() + "§8: §a" + new AdminHelperAPI(this).getIP(p));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$3IPv4 of $e" + p.getName() + "$8: $a" + new AdminHelperAPI(this).getIP(p)));
 			}
 		}
 		if (cmd.getName().equalsIgnoreCase("getping")) {
 			if (args.length == 0) {
 				if (sender instanceof Player) {
 					Player p = (Player) sender;
-					if (!p.hasPermission("adminhelper.getping")) {p.sendMessage("§cYou don't have the permission to do that.");return true;}
-					p.sendMessage("§3Ping of §e" + p.getName() + "§8: §a" + new AdminHelperAPI(this).getPing(p));
+					if (!p.hasPermission("adminhelper.getping")) {p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cYou don't have the permission to do that."));return true;}
+					p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$3Ping of $e" + p.getName() + "$8: $a" + new AdminHelperAPI(this).getPing(p)));
 				} else {
 					sender.sendMessage("/getping <Player>");
 				}
 			} else if (args.length == 1) {
-				if (!sender.hasPermission("adminhelper.getping.others")) {sender.sendMessage("§cYou don't have the permission to do that.");return true;}
+				if (!sender.hasPermission("adminhelper.getping.others")) {sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cYou don't have the permission to do that."));return true;}
 				OfflinePlayer of = Bukkit.getOfflinePlayer(args[0]);
 				if (!of.hasPlayedBefore()) {
-					sender.sendMessage("§cThis player never played before on this server.");
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cThis player never played before on this server."));
 					return true;
 				}
 				if (!of.isOnline()) {
-					sender.sendMessage("§cThis player is not online.");
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cThis player is not online."));
 					return true;
 				}
 				Player p = (Player) of;
-				p.sendMessage("§3Ping of §e" + p.getName() + "§8: §a" + new AdminHelperAPI(this).getPing(p));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$3Ping of $e" + p.getName() + "$8: $a" + new AdminHelperAPI(this).getPing(p)));
 			}
 		}
 		if (cmd.getName().equalsIgnoreCase("getlang")) {
 			if (args.length == 0) {
 				if (sender instanceof Player) {
 					Player p = (Player) sender;
-					if (!p.hasPermission("adminhelper.getlang")) {p.sendMessage("§cYou don't have the permission to do that.");return true;}
-					p.sendMessage("§3Selected language of §e" + p.getName() + "§8: §a" + new AdminHelperAPI(this).getLanguage(p));
+					if (!p.hasPermission("adminhelper.getlang")) {p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cYou don't have the permission to do that."));return true;}
+					p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$3Selected language of $e" + p.getName() + "$8: $a" + new AdminHelperAPI(this).getLanguage(p)));
 				} else {
 					sender.sendMessage("/getlang [Player]");
 				}
 			} else if (args.length == 1) {
-				if (!sender.hasPermission("adminhelper.getlang.others")) {sender.sendMessage("§cYou don't have the permission to do that.");return true;}
+				if (!sender.hasPermission("adminhelper.getlang.others")) {sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cYou don't have the permission to do that."));return true;}
 				OfflinePlayer of = Bukkit.getOfflinePlayer(args[0]);
 				if (!of.hasPlayedBefore()) {
-					sender.sendMessage("§cThis player never played before on this server.");
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cThis player never played before on this server."));
 					return true;
 				}
 				if (!of.isOnline()) {
-					sender.sendMessage("§cThis player is not online.");
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('$', "$cThis player is not online."));
 					return true;
 				}
 				Player p = (Player) of;
-				p.sendMessage("§3Selected language of §e" + p.getName() + "§8: §a" + new AdminHelperAPI(this).getLanguage(p));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('$', "$3Selected language of $e" + p.getName() + "$8: $a" + new AdminHelperAPI(this).getLanguage(p)));
 			}
 		}
 		return true;
